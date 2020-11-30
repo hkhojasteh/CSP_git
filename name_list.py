476782c1a995de8185ff0c64507a86d229ac9fe3
Hafez Shabrang <hafez.shabrang@yahoo.com>
2020-11-30 19:47:11 +0330

    Hafez-Shabrang added his name

--- name_list.py
+++ name_list.py
@@ -1,4 +1,4 @@
-names = ["Hadi","Nasher","morteza", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan"]
+names = ["Hadi","Nasher","morteza", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang"]
 
 for n in names:
     print(n)
