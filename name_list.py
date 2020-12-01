8e74523858c3e6736b9f7f64d4f81cab94eb49d2
mortezatto <75183104+mortezatto@users.noreply.github.com>
2020-12-01 10:54:11 +0330

    Update name_list.py
    edit name

--- name_list.py
+++ name_list.py
@@ -1,4 +1,5 @@
-names = ["Hadi","Nasher","morteza", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang"]
+names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang"]
+# i edit my name
 
 for n in names:
     print(n)
