44d088d21c2eecb2e06dc4415c4a641a8c77e248
aaaaysan <68744816+aaaaysan@users.noreply.github.com>
2020-12-01 13:02:18 -0800

    Update name_list.py

--- name_list.py
+++ name_list.py
@@ -1,4 +1,4 @@
-names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar"]
+names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh"]
 
 for n in names:
     print(n)
