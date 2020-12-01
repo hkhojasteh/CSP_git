84d6a901d1197b067afcd21942a7f7c9d7ea9a56
Hadi Abdi Khojasteh <hadiabdikhojasteh@gmail.com>
2020-12-01 17:33:21 +0330

    Update name_list.py

--- name_list.py
+++ name_list.py
@@ -1,5 +1,4 @@
 names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar"]
-# i edit my name
 
 for n in names:
     print(n)
