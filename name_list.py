5b32799b63956b99a70b3606b34aaad68819bc34
azizisotudeh <azizi.sotudeh@iasbs.ac.ir>
2020-12-02 10:13:09 -0800

    Maedeh Azizi Sotudeh

--- name_list.py
+++ name_list.py
@@ -1,4 +1,4 @@
-names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh","danial abyazi"]
+names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh","danial abyazi","Maedeh Azizi Sotudeh"]
 
 for n in names:
     print(n)
