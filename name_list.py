651a8f582d48dee4438be89afe1ef33fd48bb9a5
Sara Hasanloo Nikfar <maral.nikfar@gmail.com>
2020-12-02 14:44:45 +0330

    sara changed for train

--- name_list.py
+++ name_list.py
@@ -1,4 +1,4 @@
-names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar"]
+names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar2"]
 # i edit my name
 
 for n in names:
