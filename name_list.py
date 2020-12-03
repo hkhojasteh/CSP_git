679e65e23843da16682ecfca3720c2a56b5b644b
zhalefaramgmailcom <75404108+zhalefaramgmailcom@users.noreply.github.com>
2020-12-03 16:21:52 +0330

    Update name_list.py

--- name_list.py
+++ name_list.py
@@ -2,7 +2,7 @@ names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
          "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
-         "asal safavi","amir mahdi zhalefar","ahmad madady", "Kiana"]
+         "asal safavi",,"amir mahdi zhalefar","amir mahdi zhalefar","ahmad madady", "Kiana"]
 
 for n in names:
     print(n)
