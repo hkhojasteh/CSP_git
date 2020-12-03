dc19988078eeff8fa71626bed0dabab19a363357
AmirMahdiZhalefar <amirmahdizhalefar@iasbs.ac.ir>
2020-12-03 15:17:42 +0330

    amir mahdi zhalefar changed

--- name_list.py
+++ name_list.py
@@ -2,7 +2,7 @@ names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
          "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
-         "asal safavi","ahmad madady", "Kiana"]
+         "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiana"]
 
 for n in names:
     print(n)
