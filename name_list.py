8d85f7e7f81f1278f135d6f12f5c99e26cec70f1
ahmad.madady <ahmad.madady@gmail.com>
2020-12-03 08:34:38 +0330

    ahmad

--- name_list.py
+++ name_list.py
@@ -1,4 +1,8 @@
-names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini", "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan", "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh","danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi","asal safavi"]
+names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
+         "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan",
+         "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
+         "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
+         "asal safavi","ahmad madady"]
 
 for n in names:
     print(n)
