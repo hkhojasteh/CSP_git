b1d28f02a3a2feb1e32705e6f47d85ba6a7e3c12
faridehen <75100274+faridehen@users.noreply.github.com>
2020-12-16 18:09:53 +0330

    update CSP_Git
    Practicing

--- name_list.py
+++ name_list.py
@@ -3,7 +3,7 @@ names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
          "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiana", "Milad Mohammadi",
-         "Hossein Rezaei", "Farideh Saadati"]
+         "Hossein Rezaei","Farideh Saadati"]
 
 for n in names:
     print(n)
