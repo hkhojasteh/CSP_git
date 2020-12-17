16f87f417165e0b682c6bed557ceb27771eb7f29
@gheisariyeh <a.gheisariyeh25@gmail.com>
2020-12-17 15:45:49 -0600

    Afsaneh Gheisariyeh

--- name_list.py
+++ name_list.py
@@ -3,7 +3,7 @@ names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
          "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiana", "Milad Mohammadi",
-         "Hossein Rezaei", "Fatemeh Ghaffari"]
+         "Hossein Rezaei", "Fatemeh Ghaffari","Afsaneh Gheisariyeh"]
 
 for n in names:
     print(n)
