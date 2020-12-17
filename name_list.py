1f043fbdc878b5dd049179593304b5903244eb08
Mohammad Reza Haghighat <haghighatmr@gmail.com>
2020-12-18 01:50:16 +0330

    Haghighat Added Again

--- name_list.py
+++ name_list.py
@@ -3,7 +3,7 @@ names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
          "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiana", "Milad Mohammadi",
-         "Hossein Rezaei", "Fatemeh Ghaffari", "Parisa Shirali", "paria haji abolfath"]
+         "Hossein Rezaei", "Fatemeh Ghaffari", "Parisa Shirali", "paria haji abolfath", "Mohammad Reza Haghighat"]
 
 for n in names:
     print(n)
