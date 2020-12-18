4a6d506513bab2aa8df0aa9aa9fac86e8d1121e0
Matin <mmgn47@gmail.com>
2020-12-19 00:03:40 +0330

    Ebrahimkhani Added.

--- name_list.py
+++ name_list.py
@@ -1,10 +1,10 @@
-names = ["Hadi","Nasher","morteza-abedi", "Hamidhkoeini",
+names = ["Matin Ebrahimkhani","Nasher","morteza-abedi", "Hamidhkoeini",
          "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
          "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiaena", "Milad Mohammadi ","Farideh Saadati"
          "Hossein Rezaei", "Fatemeh Ghaffari", "Parisa Shirali", "paria haji abolfath",
-         "Afsaneh Gheisariyeh", "Mohammad Reza Haghighat", "Farideh"," Hossein Sadeghi"]
+         "Afsaneh Gheisariyeh", "Mohammad Reza Haghighat", "Farideh","Hadi"," Hossein Sadeghi"]
 
 for n in names:
     print(n)
