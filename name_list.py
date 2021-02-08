d6b1408bb541fe8ae5e320bbab3e8bb5dc34b716
arv78 <arv78@ymail.com>
2021-02-08 17:06:32 +0330

    added my name:Arya Azimi to the name list

--- name_list.py
+++ name_list.py
@@ -1,4 +1,4 @@
-names = ["Matin Ebrahimkhani","Nasher","morteza-abedi", "Hamidhkoeini",
+names = ["Arya Azimi", "Matin Ebrahimkhani","Nasher","morteza-abedi", "Hamidhkoeini",
          "Mohsen Mousavi", "Elnazjangi", "Mostafa Dehghan",
          "Hafez Shabrang", "Sara H. Nikfar","Aysan Chehreh",
          "danial abyazi","Maedeh Azizi Sotudeh","hadi rahimi",
