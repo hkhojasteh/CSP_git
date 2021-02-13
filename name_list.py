e2c3f56c123caf2067c79a1c23622e5cbd125382
ErfanBakhtiari <79022487+ErfanBakhtiari@users.noreply.github.com>
2021-02-13 23:22:19 +0330

    Update name_list.py

--- name_list.py
+++ name_list.py
@@ -5,7 +5,7 @@ names = ["Arya Azimi", "Matin Ebrahimkhani","Nasher","morteza-abedi", "Hamidhkoe
          "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiaena", "Milad Mohammadi ","Farideh Saadati"
          "Hossein Rezaei", "Fatemeh Ghaffari", "Parisa Shirali", "paria haji abolfath",
          "Afsaneh Gheisariyeh", "Mohammad Reza Haghighat", "Farideh","Hadi"," Hossein Sadeghi", "Solmaz Mohammadi", "Masih Hajsaeedi", "Ali Derogar","Fateme Sohrabi", 
-         "Mehrzad Tareh","Sepide","Hadi Nasiri","Shayan Majidifar", "Arash Bahmani", "Fatemeh Mohammadi","Ali Rezapour", "Amirhossein Mafi","Atefeh Malek","Maedeh Saadati", "Alireza KamkarRouhani","Mohammad Hosseinali"]
+         "Mehrzad Tareh","Sepide","Hadi Nasiri","Shayan Majidifar", "Arash Bahmani", "Fatemeh Mohammadi","Ali Rezapour", "Amirhossein Mafi","Atefeh Malek","Maedeh Saadati", "Alireza KamkarRouhani","Mohammad Hosseinali","Erfan Bakhtiari"]
 
 for n in names:
     print(n)
