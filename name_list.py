a9c43f30d537d6e380210e3fcf00ca12763b876f
maedeh-sdti <79015482+maedeh-sdti@users.noreply.github.com>
2021-02-13 18:42:06 +0200

    Update name_list.py

--- name_list.py
+++ name_list.py
@@ -5,7 +5,7 @@ names = ["Arya Azimi", "Matin Ebrahimkhani","Nasher","morteza-abedi", "Hamidhkoe
          "asal safavi","Amir mahdi zhalefar","ahmad madady", "Kiaena", "Milad Mohammadi ","Farideh Saadati"
          "Hossein Rezaei", "Fatemeh Ghaffari", "Parisa Shirali", "paria haji abolfath",
          "Afsaneh Gheisariyeh", "Mohammad Reza Haghighat", "Farideh","Hadi"," Hossein Sadeghi", "Solmaz Mohammadi", "Masih Hajsaeedi", "Ali Derogar","Fateme Sohrabi", 
-         "Mehrzad Tareh","Sepide","Hadi Nasiri"]
+         "Mehrzad Tareh","Sepide","Hadi Nasiri","Maedeh Saadati"]
 
 for n in names:
     print(n)
